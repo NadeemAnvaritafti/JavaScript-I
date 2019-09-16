5e9e8c36947b87e3d74547b9d62e11b4d91e66b4
Nadeem Anvaritafti <nanvari@live.com>
2019-09-16 16:51:04 -0500

    MVP and Stretch assignments complete for JS1

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,21 +4,37 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
+let myFunction = () => 
+    console.log("Function was invoked!");
+
+myFunction();
+
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+let anotherFunction = (param) => param;
+anotherFunction('argument');
+console.log(anotherFunction('argument'));
+
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+let add = (param1, param2) => param1 + param2;
+add(1,8);
+console.log(add(1, 8));
+
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+let subtract = (param1, param2) => param1 - param2;
+subtract(17, 7);
+console.log(subtract(17, 7));
 
 
 // Stretch
@@ -28,3 +44,8 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
+exampleArray = [1, 2, 3, 4]
+const triple = exampleArray.map(num => num * 3);
+
+console.log(triple);
